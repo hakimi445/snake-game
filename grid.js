dc60ead82b7801ae97fd7e2a1b080a1b455d719b
hakimi445 <amirulafifhakimi@gmail.com>
2024-08-26 16:18:34 +0800

    Update grid.js

--- grid.js
+++ grid.js
@@ -1,15 +1,15 @@
-const GRIDE_SIZE = 21
+const GRID_SIZE = 21
 
-export funtion randomGridPosition() {
+export function randomGridPosition() {
   return {
     x: Math.floor(Math.random() * GRID_SIZE) + 1,
-    y:  Math.floor(Math.random() * GRID_SIZE) + 1
-  }                               
-}  
+    y: Math.floor(Math.random() * GRID_SIZE) + 1
+  }
+}
 
 export function outsideGrid(position) {
-  return(
+  return (
     position.x < 1 || position.x > GRID_SIZE ||
-    position.y < 1 || position.y > GRIDE_SIZE
-    )
+    position.y < 1 || position.y > GRID_SIZE
+  )
 }
