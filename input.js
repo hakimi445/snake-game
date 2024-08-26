e30e85bf4160953fac7a8c9a8fd6cccdcba86915
hakimi445 <amirulafifhakimi@gmail.com>
2024-08-26 16:17:24 +0800

    Update input.js

--- input.js
+++ input.js
@@ -7,20 +7,20 @@ window.addEventListener('keydown', e => {
       if (lastInputDirection.y !== 0) break
       inputDirection = { x: 0, y: -1 }
       break
- case 'ArrowDown':
+    case 'ArrowDown':
       if (lastInputDirection.y !== 0) break
+      inputDirection = { x: 0, y: 1 }
+      break
+    case 'ArrowLeft':
+      if (lastInputDirection.x !== 0) break
       inputDirection = { x: -1, y: 0 }
       break
- case 'ArrowLeft':
-  if (lastInputDirection.x !== 0) break
-  inputDirection = { x: -1, y: 0 }
-  break 
-  case 'ArrowRight':
-  if (lastInputDirection.x !== 0) break
-  inputDirection = { x: 1, y: 0 }
-  break
-}
-})                   
+    case 'ArrowRight':
+      if (lastInputDirection.x !== 0) break
+      inputDirection = { x: 1, y: 0 }
+      break
+  }
+})
 
 export function getInputDirection() {
   lastInputDirection = inputDirection
